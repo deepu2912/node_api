5b704a5389277bed1aa4cc540286a2e70ee570a4
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-08-11 18:08:33 +0530

    Add api email

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 const express = require('express');
-
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-res.send('Hello welcome to our api')
+router.get('/email/:name/:cemail/:cphone/:csubject/:cmessage',(request,res)=>{
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'deepanshu2912@gmail.com',
+          pass: 'Priya@1996'
+        }
+      });
+      var name = request.params.name;
+      var cemail = request.params.cemail;
+      var cphone = request.params.cphone;
+      var csubject = request.params.csubject;
+      var cmessage = request.params.cmessage;
+
+      var mailOptions = {
+        from: cemail,
+        to: 'deepanshu2912@gmail.com',
+        subject:  csubject,
+        text: name + 'has trying to contact you. \n' + 'Please reach us at : ' + name 
+        + '\n' + cphone + '\n'  + cemail + '\n'  + cmessage 
+      };
+
+      var mailOptions2 = {
+        from: 'deepanshu2912@gmail.com',
+        to: cemail,
+        subject:  'Thanks',
+        text: 'Thanks for contacting us.' 
+      };
+      
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+      transporter.sendMail(mailOptions2, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+    res.send('Email Send Successfully')
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,15 +10,7 @@ app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/',indexRouter);
-
-// const mongoose = require('mongoose');
-// mongoose.connect(process.env.DATABASE_URL,{
-//     useNewUrlParser:true
-// })
-
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('connected to mongoose'))
+ 
 
 
 app.listen(process.env.PORT || 9000);

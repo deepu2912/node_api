072286b409b5c75694214b12a0e41a79efdd367c
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-08-12 17:28:45 +0530

    Add query save

--- routes/index.js
+++ routes/index.js
@@ -2,53 +2,118 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const router = express.Router();
 
-router.get('/email/:name/:cemail/:cphone/:csubject/:cmessage',(request,res)=>{
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'deepanshu2912@gmail.com',
-          pass: 'Priya@1996'
-        }
-      });
-      var name = request.params.name;
-      var cemail = request.params.cemail;
-      var cphone = request.params.cphone;
-      var csubject = request.params.csubject;
-      var cmessage = request.params.cmessage;
+const fs = require('fs');
+const { request } = require('http');
+const { json } = require('express');
 
-      var mailOptions = {
-        from: cemail,
-        to: 'deepanshu2912@gmail.com',
-        subject:  csubject,
-        text: name + 'has trying to contact you. \n' + 'Please reach us at : ' + name 
-        + '\n' + cphone + '\n'  + cemail + '\n'  + cmessage 
-      };
 
-      var mailOptions2 = {
-        from: 'deepanshu2912@gmail.com',
-        to: cemail,
-        subject:  'Thanks',
-        text: 'Thanks for contacting us.' 
-      };
-      
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
+
+router.get('/email/:name/:cemail/:cphone/:csubject/:cmessage', (request, res) => {
+ 
+  fs.exists('query.json', function (exists) {
+
+    if (exists) {
+
+      console.log("yes file exists");
+
+      fs.readFile('query.json', function readFileCallback(err, data) {
+
+        if (err) {
+
+          console.log(err);
+
         } else {
-          console.log('Email sent: ' + info.response);
+  
+          var obj = {
+            name: request.params.name,
+            cemail: request.params.cemail,
+            cphone: request.params.cphone,
+            csubject: request.params.csubject,
+            cmessage: request.params.cmessage,
+          }
+         
+          var data2 = JSON.parse(data);
+
+          data2.push(obj);
+          let json = JSON.stringify(data2);
+          fs.writeFileSync('query.json', json);
         }
       });
+    } else {
 
-      transporter.sendMail(mailOptions2, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+      console.log("file not exists");
 
-    res.send('Email Send Successfully')
+      var obj = {
+        name: request.params.name,
+        cemail: request.params.cemail,
+        cphone: request.params.cphone,
+        csubject: request.params.csubject,
+        cmessage: request.params.cmessage,
+      }
+
+      var json_Data = [];
+      
+      json_Data.push(obj)
+ 
+      let json = JSON.stringify(json_Data);
+ 
+      fs.writeFileSync('query.json', json);
+    }
+  });
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'deepanshu2912@gmail.com',
+      pass: 'Priya@1996'
+    }
+  });
+  var name = request.params.name;
+  var cemail = request.params.cemail;
+  var cphone = request.params.cphone;
+  var csubject = request.params.csubject;
+  var cmessage = request.params.cmessage;
+
+  var mailOptions = {
+    from: cemail,
+    to: 'deepanshu2912@gmail.com',
+    subject: csubject,
+    text: name + 'has trying to contact you. \n' + 'Please reach us at : ' + name
+      + '\n' + cphone + '\n' + cemail + '\n' + cmessage
+  };
+
+  var mailOptions2 = {
+    from: 'deepanshu2912@gmail.com',
+    to: cemail,
+    subject: 'Thanks',
+    text: 'Thanks for contacting us.'
+  };
+
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  transporter.sendMail(mailOptions2, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  res.send('Email Send Successfully')
+})
+
+
+router.get('/queries',(request,res)=>{
+  var rawdata =  fs.readFileSync('query.json');
+  rawdata = JSON.parse(rawdata)
+res.send(rawdata)
 })
 
 module.exports = router;
